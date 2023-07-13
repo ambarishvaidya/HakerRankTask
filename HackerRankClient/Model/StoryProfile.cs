a3ca1312905fe93f92ff45d33311c6a05d142da5
Ambarish Vaidya <ambarish.vaidya@gmail.com>
2023-07-13 18:34:45 +0100

    Formatted required time string.

--- HackerRankClient/Model/StoryProfile.cs
+++ HackerRankClient/Model/StoryProfile.cs
@@ -5,6 +5,7 @@ namespace HackerNewsClient.Model
     public class StoryProfile : Profile
     {
         private static IMapper mapper;
+        private static string DateFormat = @"yyyy-MM-dd'T'HH:mm:sszzz";
         static StoryProfile()
         {
             var config = new MapperConfiguration(cfg =>
@@ -12,7 +13,8 @@ namespace HackerNewsClient.Model
                 cfg.CreateMap<Story, StoryReadDto>()
                 .ForMember(dto => dto.uri, opts => opts.MapFrom(src => src.url))
                 .ForMember(dto => dto.postedBy, opts => opts.MapFrom(src => src.by))
-                .ForMember(dto => dto.commentCount, opts => opts.MapFrom(src => src.descendants));
+                .ForMember(dto => dto.commentCount, opts => opts.MapFrom(src => src.descendants))
+                .ForMember(dto => dto.time, opts => opts.MapFrom(src => DateTimeOffset.FromUnixTimeSeconds(src.time).ToString(DateFormat)));
             });
             mapper = config.CreateMapper();
         }        
